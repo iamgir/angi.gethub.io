412a8b91b5566686e952b7cf2d985b77f9cde1cf
iamgir <61762926+iamgir@users.noreply.github.com>
2020-03-03 18:50:23 -0500

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,24 +1,24 @@
 <?php
-    $name = $_Post['name'];
-    $vister_email = $_Post['email'];
-    $message = $_Post['message'];
-
-
-    $email_from = 'adkhuon@gmail.com';
-
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name.\n".
-                        "User Email: $vistor_email.\n".
-                            "User Message: $message.\n";
-
-    $mailTo = "angiphotos.info@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-
-    $headers = "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
+require('phpmailer/class.phpmailer.php');
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port     = 587;  
+$mail->Username = "adkhuon";
+$mail->Password = "\p4anD4/7197#";
+$mail->Host     = "smtp.gmail.com";
+$mail->Mailer   = "smtp";
+$mail->SetFrom("Your from email", "from name");
+$mail->AddReplyTo("from email", "PHPPot");
+$mail->AddAddress("recipient email");
+$mail->Subject = "Test email using PHP mailer";
+$mail->WordWrap   = 80;
+$content = "<b>This is a test email using PHP mailer class.</b>"; $mail->MsgHTML($content);
+$mail->IsHTML(true);
+if(!$mail->Send()) 
+echo "Problem sending email.";
+else 
+echo "email sent.";
 ?>
